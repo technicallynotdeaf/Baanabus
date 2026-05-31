6e6d7f477b010fee498d225311f6d8c1075ae3eb
Alison Keen <ali.keen@gmail.com>
2026-05-31 17:51:09 +1000

    Fix record_question_seen missing config_helper require

--- api/record_question_seen.php
+++ api/record_question_seen.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/../init.php';
+require_once __DIR__ . '/../config_helper.php';
 header('Content-Type: application/json; charset=utf-8');
 
 if (!isAuthenticated()) json_response(['error' => 'Not authenticated'], 401);
